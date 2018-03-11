5f8ba42000439d6df60200cac47a1f2a3792cc83
jagjeet0824 <jagjeet0824@gmail.com>
2018-03-11 12:49:09 -0400

    first commit

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+// jQuery 
+        $(document).ready(function(){
+            // jQuery used for onclick event
+            $("#slider").click(function(event){
+                //  Moves the button To Glow
+                $("h1").toggleClass("on");
+            });
+        });
+       
